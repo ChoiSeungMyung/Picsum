c37849559c4da6d7a55bb113f77b0af8b409faf2
Seungmyung <seungmyung.choi@riiid.co>
2024-04-02 08:52:49 +0900

    :seedling: Add network reponse and photo data to model

---
+++ model/src/main/kotlin/com/example/lookpin/model/entity/Photo.kt
@@ -0,0 +1,11 @@
+package com.example.lookpin.model.entity
+
+data class Photo(
+    val id: String,
+    val author: String,
+    val width: Int,
+    val height: Int,
+    val url: String,
+    val downloadUrl: String,
+    val checked: Boolean,
+)
