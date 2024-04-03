1d1339e3f03cb04f68aedfce9e3ba907f7eff9da
Seungmyung <seungmyung.choi@riiid.co>
2024-04-03 13:13:24 +0900

    :pencil2: Rename picsum to photo

--- data/src/main/kotlin/com/example/lookpin/data/di/DataModule.kt
+++ data/src/main/kotlin/com/example/lookpin/data/di/DataModule.kt
@@ -1,7 +1,7 @@
 package com.example.lookpin.data.di
 
-import com.example.lookpin.data.repository.PicsumRepository
-import com.example.lookpin.data.repository.impl.DefaultPicsumRepository
+import com.example.lookpin.data.repository.PhotoRepository
+import com.example.lookpin.data.repository.impl.DefaultPhotoRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -13,7 +13,7 @@ import javax.inject.Singleton
 abstract class DataModule {
     @Binds
     @Singleton
-    abstract fun bindPicsumRepository(
-        impl: DefaultPicsumRepository,
-    ): PicsumRepository
+    abstract fun bindPhotoRepository(
+        impl: DefaultPhotoRepository,
+    ): PhotoRepository
 }
---
+++ data/src/main/kotlin/com/example/lookpin/data/repository/impl/DefaultPhotoRepository.kt
@@ -0,0 +1,59 @@
+package com.example.lookpin.data.repository.impl
+
+import com.example.lookpin.data.remote.source.PhotoRemoteSource
+import com.example.lookpin.data.repository.PhotoRepository
+import com.example.lookpin.model.entity.Photo
+import com.example.lookpin.model.mapper.toData
+import javax.inject.Inject
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.getAndUpdate
+import kotlinx.coroutines.flow.mapLatest
+import kotlinx.coroutines.flow.onStart
+
+class DefaultPhotoRepository @Inject constructor(
+    private val remote: PhotoRemoteSource,
+) : PhotoRepository {
+
+    private val photosStateFlow =
+        MutableStateFlow<List<Photo>>(emptyList())
+
+    override fun photos(): Flow<List<Photo>> {
+        return photosStateFlow.onStart {
+            if (photosStateFlow.value.isEmpty()) {
+                refresh()
+            }
+        }
+    }
+
+    override suspend fun refresh() {
+        remote
+            .getPhotos()
+            .map { photoResponse ->
+                photoResponse.toData()
+            }
+            .let {
+                photosStateFlow.value = it
+            }
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    override fun getPhotoById(id: String): Flow<Photo?> =
+        photosStateFlow.mapLatest { list ->
+            list.firstOrNull { it.id == id }
+        }
+
+    override fun updatePhotos(list: List<Photo>) {
+        photosStateFlow.value = list
+    }
+
+    override fun updatePhoto(photo: Photo) {
+        photosStateFlow.getAndUpdate { list ->
+            list.map {
+                if (it.id == photo.id) it.copy(checked = !it.checked)
+                else it
+            }
+        }
+    }
+}
